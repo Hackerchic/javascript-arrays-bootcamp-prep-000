ef9059a18eddb82e34405da2b8d5f73b4f64b720
Sara Grimes <sarajoy415@gmail.com>
2019-07-01 21:49:25 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,46 @@
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+
+var grilledCheeseIngredients = [
+  "Bread",
+  "mild cheese",
+  "sharp cheese",
+  "butter",
+  "tomato"
+  "garlic"]
+  
+var tomatoSuaceIngred = [ 
+  "tomato",
+  "garlic,"
+  "olive oil",
+  "basil",
+  "oregono"]
+  
+  var evenNumbers =
+  new Array();
+  
+  var chocolateBars = [
+    "snickers,"
+    "hundred grand",
+    "kitkat,"
+    "skittles"]
+    
+   chocolateBars.push("snickerdoodle");
+    
+    chocolateBars.unshift("bonbons")
+    
+    function addElementToBeginningOfArray(chocolateBars, champagne){
+    return ["champagne"...chocolateBars]}
+    
+    function destructivelyAddElementToBeginningOfArray(chocolateBars, cryptonite)
+    return chocolateBars = [cryptonite + cities]}
+    
+  var entrepreneurs = ["Oprah Winfrey", "Laurene Powell Jobs", "Arianna Huffington"];  
+    function accessELementInArray("Oprah Winfrey", entrepreneurs[1])
+    return "Oprah Winfrey"[1]
+    
+    
